b470c4144dffec5ab86923d123ef38205f891871
Paul Gowder <paul.gowder@gmail.com>
2019-06-06 15:09:53 -0500

    Initial Commit

---
+++ speedScraper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  speedScraper
+//
+//  Created by Paul Gowder on 6/6/19.
+//  Copyright © 2019 Paul Gowder. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ speedScraper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  speedScraper
+//
+//  Created by Paul Gowder on 6/6/19.
+//  Copyright © 2019 Paul Gowder. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
