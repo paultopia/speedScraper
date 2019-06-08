1d04533a46262402241c84acc305cceb7b1f11d8
Paul Gowder <paul.gowder@gmail.com>
2019-06-08 13:52:22 -0500

    get link data structure cleaned up

--- speedScraper/links.swift
+++ speedScraper/links.swift
@@ -14,34 +14,63 @@
 
 import Foundation
 
-struct Links: Codable {
+struct InLink: Codable {
     let text: String
     let href: String
+}
+
+struct Link {
+    let text: String
+    let href: String
+    let filename: String?
     
-    func defrag() -> Links? {
+    func defrag() -> Link? {
         guard let url = URL(string: href) else {
             return nil
         }
         if let fragment = url.fragment {
             let output = url.absoluteString.replacingOccurrences(of: "#\(fragment)", with: "")
-            return Links(text: text, href: output)
+            return Link(text: text, href: output, filename: filename)
         } else {
-            return Links(text: text, href: href)
+            return Link(text: text, href: href, filename: filename)
         }
     }
+}
 
+extension Link {
+    init(_ inLink: InLink){
+        text = inLink.text
+        href = inLink.href
+        filename = nil
+    }
 }
 
 struct LinkList {
-    var links: [Links]
-    init(_ inLinks: [Links]) {
-        links = inLinks
+    typealias LinksType = [Link]
+    var links: LinksType
+    init(_ links: [Link]) {
+        self.links = links
+    }
+    init(_ inLinks: [InLink]){
+        links = inLinks.map {Link($0)}
     }
 }
 
-func decodeLinks(_ inLinks: String) -> [Links] {
+func decodeLinks(_ inLinks: String) -> LinkList {
     let decoder = JSONDecoder()
-    let outLinks = try! decoder.decode([Links].self, from: inLinks.data(using: .utf8)!)
-    return outLinks
+    let outLinks = try! decoder.decode([InLink].self, from: inLinks.data(using: .utf8)!)
+    return LinkList(outLinks)
 }
 
+extension LinkList: Collection {
+    typealias Index = LinksType.Index
+    typealias Element = LinksType.Element
+    var startIndex: Index { return links.startIndex }
+    var endIndex: Index { return links.endIndex }
+    subscript(index: Index) -> Element {
+        get { return links[index] }
+    }
+    func index(after i: Index) -> Index {
+        return links.index(after: i)
+    }
+}
